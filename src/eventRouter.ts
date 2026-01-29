fcf18d624883714faf76ea1ad5583cfe4bb77f8a
起司玉米蛋餅 <ericxpert@gmail.com>
2026-01-29 23:30:32 +0800

    style: 改寫 enentRouter.ts
    原先寫法會誤導為併行執行

--- src/eventRouter.ts
+++ src/eventRouter.ts
@@ -1,9 +1,8 @@
 import { sendMessage, markAsRead, loadStart } from "./eventRoutes";
 
 export async function eventRouter(event: any, channelAccessToken: string) {
-  (await markAsRead(channelAccessToken, event.message.markAsReadToken),
-    await loadStart(channelAccessToken, event.source.userId, 5));
-
+  await markAsRead(channelAccessToken, event.message.markAsReadToken);
+  await loadStart(channelAccessToken, event.source.userId, 5);
   await sendMessage(channelAccessToken, event.replyToken, [
     {
       type: "text",
