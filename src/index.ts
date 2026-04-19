69e5bec72d6f180d84d73f4612ed31f5057275e8
起司玉米蛋餅 <ericxpert@gmail.com>
2026-04-19 15:13:53 +0800

    chore: 移除不必要的 Env 介面定義

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,6 @@ import type { webhook } from "@line/bot-sdk";
 import { hmacSHA256Base64 } from "./utils/hmacSHA256Base64";
 import { eventRouter } from "./eventRouter";
 
-interface Env {
-  LINE_CHANNEL_SECRET: {
-    get(): Promise<string>;
-  };
-  LINE_CHANNEL_ACCESS_TOKEN: {
-    get(): Promise<string>;
-  };
-}
-
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
     // 檢查方法合法
